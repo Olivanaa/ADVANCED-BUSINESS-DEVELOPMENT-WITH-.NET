cd5be4149dbddc7171f5565158bfc53ca4de6da0
olivanaa <oliv.anaclara@gmail.com>
2024-02-23 09:59:22 -0300

    aula 2302

---
+++ MVC-main/Models/Endereco.cs
@@ -0,0 +1,8 @@
+﻿namespace _2TDSPK.Models
+{
+    public class Endereco : Auditoria
+    {
+        public int Id { get; set; }
+        public string Cep { get; set; }
+    }
+}
---
+++ MVC-main/Models/Professor.cs
@@ -0,0 +1,8 @@
+﻿namespace _2TDSPK.Models
+{
+    public class Professor : Usuario
+    {
+        public int Id { get; set; }
+
+    }
+}
